44d7d8c1fc6b1d7ccfdc1edb38407f72a5dc0e4a
TareqSheikh11 <js1720619gmail.com>
2021-10-12 09:25:57 +0600

    10 October 21, 2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-String BtmText = "Button 1";
+double imgHight = 150;
+double imgwight = 150;
+bool imgvsblty1 = false;
+bool imgvsblty2 = false;
+String btnText1 = "Button One ";
+String btnText2 = "Button Two";
+String btnText3 = "Button Three";
+String imgSrc1 = "https://cdn.pixabay.com/photo/2016/02/17/19/08/lotus-1205631_960_720.jpg";
+Color btnColor1 = Colors.green;
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -30,39 +38,135 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text("My App"),
       ),
-      body: SingleChildScrollView(
+      body:
+      SingleChildScrollView(
         child: Center(
           child: Column(
 
             children: [
+
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                 children: [
+                   RaisedButton(onPressed: (){
+
+                   },
+                     child: Text(btnText1) ,
+
+                   ),
+                   Container(
+                     decoration: BoxDecoration(
+                       image: DecorationImage(image: NetworkImage(imgSrc1),fit: BoxFit.cover),
+                       border: Border.all(color: Colors.lightGreen, width: 4),
+                       borderRadius: BorderRadius.all(Radius.circular(40),),),
+                       height: imgHight,
+                       width: imgwight,
+                       //child: Image.network(imgSrc1,fit: BoxFit.cover)
+                   ),
+                   RaisedButton(onPressed: (){
+
+                   },
+                     child: Text(btnText2) ,
+
+                   ),
+                   Container(
+                     decoration: BoxDecoration(
+                       image: DecorationImage(image: NetworkImage(imgSrc1),fit: BoxFit.cover),
+                       border: Border.all(color: Colors.lightGreen, width: 4),
+                       borderRadius: BorderRadius.all(Radius.circular(40),),),
+                     height: imgHight,
+                     width: imgwight,
+                   ),
+                   Container(
+                       height: imgHight,
+                       width: imgwight,
+                       child: Image.network(imgSrc1)
+                   ),
+                   Container(
+                       height: imgHight,
+                       width: imgwight,
+                       child: Image.network(imgSrc1)
+                   ),
+                   Container(
+                       height: imgHight,
+                       width: imgwight,
+                       child: Image.network(imgSrc1)
+                   ),
+                 ],
+                ),
+              ),
+
+
+
+
+
               SizedBox(
                 height: 15,
               ),
               RaisedButton(
-                  child: Text("Button One"),
+                color: btnColor1,
+                  child: Text(btnText1),
                   onPressed: (){
+                    setState(() {
+                      imgvsblty1=true;
+                      btnColor1 = Colors.redAccent ;
+                      btnText1 = "Button Pressed";
+                    });
 
     }),
-              Container(
-                height: 300,
-                  width: 400,
-                  child: Image.network(
-                      "https://cdn.pixabay.com/photo/2016/02/17/19/08/lotus-1205631_960_720.jpg"
-                  )),
-              Text("Fig 1: Water Lily"),
+              Visibility(
+                visible: imgvsblty1,
+                child: Container(
+                  height: 300,
+                    width: 400,
+                    child: Image.network(
+                        "https://cdn.pixabay.com/photo/2016/02/17/19/08/lotus-1205631_960_720.jpg"
+                    )),
+              ),
+              Visibility(
+                visible: imgvsblty1,
+                  child: Text("Fig 1: Water Lily")),
               SizedBox(
                 height: 15,
               ),
-              Container(
-                  height: 600,
-                  width: 400,
-                  child:
-                  Image.asset("assets/images/image1.jpeg")
+              RaisedButton(
+                child: Text(btnText2),
+                onPressed: (){
+                  setState(() {
+                    btnText2="Button Pressed";
+                    imgvsblty2=true;
+                  });
+                },
               ),
+               Visibility(
+                  visible: imgvsblty2,
+                  child: Container(
+                      height: 600,
+                      width: 400,
+                      child:
+                      Image.asset("assets/images/image1.jpeg")
+                  ),
+                ),
+
               SizedBox(
                 height: 15,
               ),
-              Text("Fig 2:Red Rose"),
+              Visibility(
+                visible: imgvsblty2,
+                  child: Text("Fig 2:Red Rose")),
+              SizedBox(
+                height: 15,
+              ),
+              RaisedButton(
+                color: Colors.red,
+                  child: Text(btnText3),
+                  onPressed:(){
+                    setState(() {
+                      btnText3="Button Pressed";
+                    });
+
+                  }),
 
   ],
           ),
